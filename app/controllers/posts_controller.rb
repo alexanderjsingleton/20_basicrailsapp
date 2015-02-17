090d4791260fa84bf9f4ff453ae0a97f8a3edf69
Alex Singleton <alexanderjsingleton@gmail.com>
2015-02-17 12:00:40 -0600

    completed module

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
- def create
+  def create
    @post = Post.new(params.require(:post).permit(:title, :body))
    if @post.save
      flash[:notice] = "Post was saved."
@@ -20,8 +20,21 @@ class PostsController < ApplicationController
      flash[:error] = "There was an error saving the post. Please try again."
      render :new
    end
- end
+  end
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+   @post = Post.find(params[:id])
+   if @post.update_attributes(params.require(:post).permit(:title, :body))
+     flash[:notice] = "Post was updated."
+     redirect_to @post
+   else
+     flash[:error] = "There was an error saving the post. Please try again."
+     render :edit
+   end
+  end
+
 end

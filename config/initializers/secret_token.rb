0e3780e69bbf0a24a7d11a51a9ed13d0aeb91398
Alex Singleton <alexanderjsingleton@gmail.com>
2015-02-19 11:27:05 -0600

    Authenticated

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Bloccit::Application.config.secret_key_base = 'e81c3ff8766296a775ee37d3d480fc3e73f928d44ce568458f371e880644bc4799832de7387399a14f9cedc947497851c4e9cbd35d639e6b74ab85c4f9fc8c3a'
+Bloccit::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']

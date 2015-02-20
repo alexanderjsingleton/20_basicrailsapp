a1f940d185084bde24ac83c84fc7e05c5740a6da
Alex Singleton <alexanderjsingleton@gmail.com>
2015-02-20 13:21:26 -0600

    Remove unnecessary code from environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,15 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Bloccit::Application.initialize!
-
-# Configuration for using SendGrid on Heroku
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :user_name => "yourSendGridusernameyougetfromheroku",
-  :password => "yourSendGridpasswordyougetfromheroku",
-  :domain => "staging.freelanceful.com",
-  :address => "smtp.sendgrid.net",
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}

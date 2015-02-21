2143057c0a1b6857d7bd861171831e89e073753a
Alex Singleton <alexanderjsingleton@gmail.com>
2015-02-21 15:55:17 -0600

    authorization/installed Pundit' .

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  
+  # This method, called on a given user, looks at the role stored for that user in the database. It returns true or false based on whether the stored role equals the expected value.         
+  # Remember the implied self? The role in this method is really self.role.
+  def admin?
+    role == 'admin'
+  end
+
+  def moderator?
+    role == 'moderator'
+  end
 end

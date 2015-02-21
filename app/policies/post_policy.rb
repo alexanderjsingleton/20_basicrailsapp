5b0fc44e97aa5f5c0be00e86343ec89980859cc5
Alex Singleton <alexanderjsingleton@gmail.com>
2015-02-21 17:39:14 -0600

    auth assignment

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,40 @@
 class PostPolicy < ApplicationPolicy
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.admin?
+        scope.all
+      elsif moderator?
+        scope.all
+      else
+        scope.where(:published => true)
+      end
+    end
+  end
+
+
   def index?
     true
   end
+
+  def update?
+    user.admin? or not post.published?
+  end
+
 end
+
+# class PostPolicy < ApplicationPolicy
+#   def permitted_attributes
+#     if user.admin? || user.owner_of?(post)
+#       [:title, :body, :tag_list]
+#     else
+#       [:tag_list]
+#     end
+#   end
+# end

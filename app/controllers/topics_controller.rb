9e4556687f10b3522a644bac0809088809d2426f
Alex Singleton <alexanderjsingleton@gmail.com>
2015-02-25 16:06:44 -0600

    Completed markdown checkpoint

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,59 +1,3 @@
-
-# class TopicsController < ApplicationController
-#   def index
-#      @topics = Topic.all
-#      authorize @topics
-#   end
-
-#   def new
-#      @topic = Topic.new
-#      authorize @topic
-#   end
-
-#   def show
-#      @topic = Topic.find(params[:id])
-#       @posts = @topic.posts
-#      authorize @topic
-#   end
-
-#   def edit
-#      @topic = Topic.find(params[:id])
-#      authorize @topic
-#   end
- 
-#    def create
-#      @topic = Topic.new(params.require(:topic).permit(:name, :description, :public))
-#      # refactor
-#      # @topic = Topic.new(params.require(:topic).permit(topic_para)
-#      authorize @topic
-#      if @topic.save
-#        redirect_to @topic, notice: "Topic was saved successfully."
-#      else
-#        flash[:error] = "Error creating topic. Please try again."
-#        render :new
-#      end
-#    end
- 
-#    def update
-#      @topic = Topic.find(params[:id])
-#      # Refactor for above
-#      # @topic = Topic.new(params.require(:topic).permit(topic_param)
-#      authorize @topic
-#      if @topic.update_attributes(params.require(:topic).permit(:name, :description, :public))
-#        redirect_to @topic
-#      else
-#        flash[:error] = "Error saving topic. Please try again."
-#        render :edit
-#      end
-#    end
-
-#   private
-
-#   def topic_params
-#     params.require(:topic).permit(:name, :description, :public)
-#   end
-
-# end
 class TopicsController < ApplicationController
   def index
     @topics = Topic.all
@@ -78,8 +22,6 @@ class TopicsController < ApplicationController
 
   def create
     @topic = Topic.new(topic_params)
-    # refactor
-    # @topic = Topic.new(params.require(:topic).permit(topic_para)
     authorize @topic
     if @topic.save
       redirect_to @topic, notice: "Topic was saved successfully."
@@ -91,8 +33,6 @@ class TopicsController < ApplicationController
 
   def update
     @topic = Topic.find(params[:id])
-    # Refactor for above
-    # @topic = Topic.new(params.require(:topic).permit(topic_param)
     authorize @topic
     if @topic.update_attributes(topic_params)
       redirect_to @topic

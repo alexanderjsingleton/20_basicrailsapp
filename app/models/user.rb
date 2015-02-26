e746fcd47c8b9319b9ea61f15f3ea5bbb8af687e
Alex Singleton <alexanderjsingleton@gmail.com>
2015-02-26 12:12:33 -0600

    Uploading Images checkpoint sans assignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
+    mount_uploader :avatar, AvatarUploader
+
   
   # This method, called on a given user, looks at the role stored for that user in the database. It returns true or false based on whether the stored role equals the expected value.         
   # Remember the implied self? The role in this method is really self.role.

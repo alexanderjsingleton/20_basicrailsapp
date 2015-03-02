a55845d779a0b763764ee1c3c09c4a6200af9693
Alex Singleton <alexanderjsingleton@gmail.com>
2015-03-02 12:06:01 -0600

    age_uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,6 +7,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
+  # storage :file
   storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -25,36 +26,25 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
-    process :resize_to_fill => [200,300]
   #
   # def scale(width, height)
   #   # do something
   # end
+  process resize_to_fill: [300, 300]
 
+  version :thumb do
+    process resize_to_fill: [50, 50]
+  end
   # Create different versions of your uploaded files:
   # version :thumb do
   #   process :resize_to_fit => [50, 50]
   # end
-    version :tiny do
-      process resize_to_fill: [20, 20]
-    end
-
-    version :small do
-      process resize_to_fill: [30, 30]
-    end
-
-    version :profile do
-      process resize_to_fill: [45, 45]
-    end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
   #   %w(jpg jpeg gif png)
   # end
-    def extension_white_list
-      %w(jpg jpeg gif png)
-    end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.

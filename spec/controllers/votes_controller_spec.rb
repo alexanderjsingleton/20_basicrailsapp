6f10722e89c36a0c76fc7831cfdd4207ab0b5ee4
Alex Singleton <alexanderjsingleton@gmail.com>
2015-03-13 14:12:57 -0500

    finished, for intents and purposes

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,4 +1,5 @@
  require 'rails_helper'
+require 'support/test_factories'
  
  describe VotesController do
  
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,6 +5,23 @@
    describe "vote methods" do
  
     include TestFactories
+    def associated_post(options={})
+     post_options = {
+       title: 'Post title',
+       body: 'Post bodies must be pretty long.',
+       topic: Topic.create(name: 'Topic name'),
+       user: authenticated_user
+     }.merge(options)
+     Post.create(post_options)
+    end
+
+    def authenticated_user(options={})
+      user_options = {email: "email#{rand}@fake.com", password: 'password'}.merge(options)
+      user = User.new(user_options)
+      user.skip_confirmation!
+      user.save
+      user
+    end
 
      before do
       #user = User.create
@@ -40,6 +57,23 @@
    end
 
     describe '#create_vote' do
+          def associated_post(options={})
+     post_options = {
+       title: 'Post title',
+       body: 'Post bodies must be pretty long.',
+       topic: Topic.create(name: 'Topic name'),
+       user: authenticated_user
+     }.merge(options)
+     Post.create(post_options)
+    end
+
+    def authenticated_user(options={})
+      user_options = {email: "email#{rand}@fake.com", password: 'password'}.merge(options)
+      user = User.new(user_options)
+      user.skip_confirmation!
+      user.save
+      user
+    end
      it "generates an up-vote when explicitly called" do
        post = associated_post
        expect( post.up_votes ).to eq(0)

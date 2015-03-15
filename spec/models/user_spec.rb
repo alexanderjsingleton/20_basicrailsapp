2b7dd49a20d798e5f1707862033861eb82645452
Alex Singleton <alexanderjsingleton@gmail.com>
2015-03-15 14:00:34 -0500

    for mentor review, as instructed by checkpoint

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,34 @@
 require 'rails_helper'
 require 'support/test_factories'
  
- describe User do
- 
-   include TestFactories
- 
-   describe "#favorited(post)" do
-     xit "returns `nil` if the user has not favorited the post" do
-     end
- 
-     xit "returns the appropriate favorite if it exists" do
-     end
-   end
- end
+ describe Comment do
+
+  include TestFactories
+
+  describe "after_create" do
+
+    before do
+      @post = associated_post
+      @user = authenticated_user
+      @comment = Comment.new(body: 'My comment', post: @post, user_id: 10000)
+    end
+
+    it "Delivers e-mail notiication after e-mailing a post" do
+      @user.favorites.where(post: @post).create
+
+      allow( FavoriteMailer )
+      .to receive(:new_comment)
+      .with(@user, @post, @comment)
+      .and_return( double(deliver: true) )
+
+      @comment.save
+    end
+
+    it "If not favorited, don't send e-mail" do
+      expect( FavoriteMailer )
+        .not_to receive(:new_comment)
+
+        @comment.save
+    end
+  end
+end

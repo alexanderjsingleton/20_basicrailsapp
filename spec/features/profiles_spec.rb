665c09421fe60b4a3a520cd0b5673f4ee60b74c8
Alex Singleton <alexanderjsingleton@gmail.com>
2015-03-21 09:42:06 -0500

    complete profiles assignment

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -1,16 +1,16 @@
  require 'rails_helper'
  require 'support/test_factories'
   
- describe "Visiting profiles" do
+  describe "Visiting profiles" do
 
-  include TestFactories
+    include TestFactories
+    include Warden::Test::Helpers
+    Warden.test_mode!
  
    before do
      @user = authenticated_user
         @post = associated_post(user: @user)
-        @comment = Comment.new(user: @user, body: "A Comment")
-        allow(@comment).to receive(:send_favorite_emails)
-        @comment.save
+        @comment = comment_without_email(post: @post, user: @user)
    end
  
    describe "not signed in" do
@@ -23,6 +23,21 @@
        expect( page ).to have_content(@post.title)
        expect( page ).to have_content(@comment.body)
      end
- 
    end
+
+   describe "user visits own" do
+
+    before do
+      login_as @user, scope: :user
+    end
+
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@comment.body)
+    end
+  end
  end

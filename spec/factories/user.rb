50cc87e2a785d745f5b50e9a73713cb9a8c9cf02
Alex Singleton <alexanderjsingleton@gmail.com>
2015-03-23 17:32:17 -0500

    resolved factories user

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
  FactoryGirl.define do
    factory :user do
-     name "Test_Name"
+     name "Bob Seger"
      sequence(:email, 100) { |n| "person#{n}@example.com" }
      password "helloworld"
      password_confirmation "helloworld"

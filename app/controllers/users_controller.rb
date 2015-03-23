30479eb56ea3535e6ea992196d171033a77cc6b1
Alex Singleton <alexanderjsingleton@gmail.com>
2015-03-23 12:43:34 -0500

    edits with Ben

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@
 
    def show
     @user = User.find(params[:id])
-      @posts = @user.posts.visible_to(current_user)
+    @posts = @user.posts.visible_to(current_user)
     @comments = @user.comments
    end
 
